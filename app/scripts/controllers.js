fcccef12ee45b3a403ff9e19fa4a163d559c79b2
atitsbest <atitsbest@gmail.com>
2014-04-23 15:54:06 +0200

    + Vorlage für Design verwendet.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,16 +1,21 @@
 angular.module('llprk')
-.controller('LoginCtrl', ['$scope', '$rootScope',
-    function($scope, $rootScope) {
+.controller('LoginCtrl', ['$scope', '$rootScope', '$location',
+    function($scope, $rootScope, $location) {
         $rootScope.title = 'Anmelden'
+
+    $scope.login = function() {
+        alert('SUBMIT');
+        $location.path('/dashboard');
+    }
     }])
 
 .controller('DashboardCtrl', ['$scope', '$rootScope',
     function($scope, $rootScope) {
         $rootScope.title = 'Deine Produkte'
         $scope.products = [
-            {name: 'Tasche 1', price: 34.99, available: 1},
-            {name: 'Tasche 2', price: 45.00, available: 1},
-            {name: 'Tasche 3', price: 54.99, available: 0},
-            {name: 'Tasche 4', price: 19.99, available: 3}
+            {name: 'Tasche 1', image: 'tasche1.png', price: 34.99, available: 1},
+            {name: 'Tasche 2', image: 'tasche1.png', price: 45.00, available: 1},
+            {name: 'Tasche 3', image: 'tasche1.png', price: 54.99, available: 0},
+            {name: 'Tasche 4', image: 'tasche1.png', price: 19.99, available: 3}
         ];
     }]);
